18bc925fa6e86addff65485efdb0e789bcbf082a
this-ILECY <this.ilecy@gmail.com>
2023-09-05 13:06:31 +0330

    before line 142

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -77,7 +77,7 @@ export class AppComponent {
     this.subscription.unsubscribe();
   }
 
-  @ViewChild('allMother', { static: true }) AlMother: ElementRef;
+  @ViewChild('allMother', { static: true }) AllMother: ElementRef;
   @ViewChild('controller', { static: true }) Controller: ElementRef;
   @Input() gridConfig: IGridConfig;
 
@@ -106,26 +106,38 @@ export class AppComponent {
 
 
 
-}
-
-
+  /*
 * if this isn't an auto-generated template,
 * and count of html element and their configs do not match,
 * throws error
 * (parent configs won't be included (IsParent = true))
-*/ configValidator() {
-let childCount = Array. from(((this .AllMother.nativeElement) as Element) children).filter(x => lx.classList.contains("controller")) . length;
-if (this.gridConfig. ElementConfig. length I= 0 && childCount I=s this gridConfig. ElementConfig.filter(x -> Ix. IsParent). length &S (Ithis.gridConfig. Auto Template))
-throw new Error("Uncaught (in promise): TypeError: count of configs did not match your page. \nat Ray-Container. InPlease make sure you have added correct configuration
-private rowCountController = 0;
-7*
+*/
+  configValidator() {
+    let childCount = Array.from(((this.AllMother.nativeElement) as Element).children).filter(x => !x.classList.contains("controller")).length;
+    if (this.gridConfig.ElementConfig.length != 0 && childCount !== this.gridConfig.ElementConfig.filter(x => !x.IsParent).length && (!this.gridConfig.AutoTemplate))
+      throw new Error("Uncaught (in promise): TypeError: count of configs did not match your page. \nat Ray-Container. InPlease make sure you have added correct configuration");
+  }
+
+  private rowCountController = 0;
+  /*
 * generating handlers, setting the configs and adding classes
-*/ pageGenerator0) {
-let allMother = (this.Al(Mother nativeElement) as Element
-this.rowCountController = getComputedStyle(allMother).gridTemplateRows.split(" "). Length;
-if (Ithis-gridConfig) return;
-(allMother as HTMLElement).style.gridTemplateColumns = 'repeat(' + this.gridConfig .Resolution + ', 1fr)';
-(allMother as HTMLElement).style.gridTemplateRows = 'repeat(' + this .gridConfig. ResolutionY + ', 1fr)'
-if (this.gridConfig. Element Config. length = 0) return;
-let generatedList = this. listGenerator(this .gridConfig.ElementConfig);
-let index = 0;
+  */
+  pageGenerator() {
+
+
+    let allMother = (this.AllMother.nativeElement) as Element
+    this.rowCountController = getComputedStyle(allMother).gridTemplateRows.split(" ").length;
+
+    if (!this.gridConfig) return;
+
+    (allMother as HTMLElement).style.gridTemplateColumns = 'repeat(' + this.gridConfig.Resolution + ', 1fr)';
+    (allMother as HTMLElement).style.gridTemplateRows = 'repeat(' + this.gridConfig.ResolutionY + ', 1fr)'
+
+    if (this.gridConfig.ElementConfig.length = 0) return;
+
+    let generatedList = this.listGenerator(this.gridConfig.ElementConfig);
+
+    let index = 0;
+
+  }
+}
