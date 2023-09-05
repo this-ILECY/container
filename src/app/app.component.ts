e5ddc579bc012c6104712903fd8ec39618f74dae
this-ILECY <this.ilecy@gmail.com>
2023-09-05 12:52:31 +0330

    before config valid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,10 @@ import { UntypedFormGroup } from '@angular/forms';
 import { AfterViewInit, Component, ElementRef, Input, OnDestroy, ViewChild } from '@angular/core';
 import { fromEvent, Subscription } from 'rxjs';
 import { take } from 'rxjs/operators';
-// import { arisEnum, ContainerDisplayllode, HandleDisplayEnun, HandlexEnum, HandleVEnum } from " . /models/container. enum";
-// import { ElementConfig, IGridConfig } from './models/grid-config-interface';
+import { arisEnum, ContainerDisplayllode, HandleDisplayEnun, HandlexEnum, HandleVEnum } from " . /models/container. enum";
+import { ElementConfig, IGridConfig } from './models/grid-config-interface';
+
+
 
 @Component({
   selector: 'app-root',
@@ -12,37 +14,96 @@ import { take } from 'rxjs/operators';
 })
 export class AppComponent {
   title = 'grid';
+
   public basics: any;
+
+
+
   private subscription: Subscription = new Subscription;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   ngOnDestroy(): void {
-    throw new Error('Method not implemented.');
+    this.subscription.unsubscribe();
   }
+
+  @ViewChild('allMother', { static: true }) AlMother: ElementRef;
+  @ViewChild('controller', { static: true }) Controller: ElementRef;
+  @Input() gridConfig: IGridConfig;
+
   ngAfterViewInit(): void {
-    throw new Error('Method not implemented.');
+
+    // this.configValidator()
+    this.pageGenerator();
+    this.basics = this.GetListOfCount();
+    if ((this.gridConfig.DisplayMode === ContainerDisplayMode.normal)) {
+      this.moveController();
+    }
+
   }
-}
 
-ngOnDestroy): void {
-this.subscription.unsubscribe();
-@ViewChild('allMother', { static: true }) AlMother: ElementRef @ViewChild( 'controller', { static: true }) Controller: ElementRef @Input() gridConfig: IGridConfig;
-ngAfterViewInit() {
-// this.configValidator()
-this.pageGenerator0);
-this.basics = this .GetListOfCount();
-if ((this.gridConfig.Displayflode s this.moveController0);
-ContainerDisplayMode.normal))
-DI
-this.metadata. fields[ 'basic'].selectOptions$ = this.basics
-}
-/*
-* get list of elements to send to metadata
-* for controller box
-*/
-public GetListOfCount(): ( id: number; name: string: 303 (
-let count = Array.from((this.AllMother.nat iveElement as HTMLElement) children).filter(x => Ix.classList.contains("controller")). length;
-let types: {
-id: number;
-name: string;
-10] = Array. from( length: count J, (value, Index) => (( id: index + 1, name: index + 1 + '° ))) return types;
+  /*
+  * get list of elements to send to metadata
+  * for controller box
+  */
 
+
+
+
+
+
+
+
+
+
+}
