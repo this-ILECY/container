b5d8a8e85c582c37a9ee75a7a55ed4c5044fb7ba
AmirHosein Mardani <139256057+this-ILECY@users.noreply.github.com>
2023-09-05 12:57:57 +0330

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -107,3 +107,25 @@ export class AppComponent {
 
 
 }
+
+
+* if this isn't an auto-generated template,
+* and count of html element and their configs do not match,
+* throws error
+* (parent configs won't be included (IsParent = true))
+*/ configValidator() {
+let childCount = Array. from(((this .AllMother.nativeElement) as Element) children).filter(x => lx.classList.contains("controller")) . length;
+if (this.gridConfig. ElementConfig. length I= 0 && childCount I=s this gridConfig. ElementConfig.filter(x -> Ix. IsParent). length &S (Ithis.gridConfig. Auto Template))
+throw new Error("Uncaught (in promise): TypeError: count of configs did not match your page. \nat Ray-Container. InPlease make sure you have added correct configuration
+private rowCountController = 0;
+7*
+* generating handlers, setting the configs and adding classes
+*/ pageGenerator0) {
+let allMother = (this.Al(Mother nativeElement) as Element
+this.rowCountController = getComputedStyle(allMother).gridTemplateRows.split(" "). Length;
+if (Ithis-gridConfig) return;
+(allMother as HTMLElement).style.gridTemplateColumns = 'repeat(' + this.gridConfig .Resolution + ', 1fr)';
+(allMother as HTMLElement).style.gridTemplateRows = 'repeat(' + this .gridConfig. ResolutionY + ', 1fr)'
+if (this.gridConfig. Element Config. length = 0) return;
+let generatedList = this. listGenerator(this .gridConfig.ElementConfig);
+let index = 0;
